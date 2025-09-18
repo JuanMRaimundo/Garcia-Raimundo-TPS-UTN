09c5975769af0822b7c1eb537296bf50343e6fa2
Supernose264 <bautigarcia264@gmail.com>
2025-09-18 16:37:11 -0300

    Leds encendidos y apagados
    Agregue unos emojis para crear la ilusion de encendido y apagado segun el orden de los 0 y 1

--- TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
+++ TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
@@ -4,7 +4,10 @@ import time  # Para usar sleep
 for numero in range(16):
     # Convertierte a binario con formato de 4 bits
     binario = format(numero, '04b')
-    # Muestra número y su representación binaria
-    print(f"{numero} -> {binario}")
-    # SE produce un retardo de 0.5 segundos para simular el conteo
+    #Esto crea la ilusion de led encendidos y apagados, esto muestran de manera visual los encendidos y apagados del codigo 
+    #🔴 para 1 y  ⚫ para 0
+    leds = ''.join('🔴' if bit == '1' else '⚫' for bit in binario)
+    #Muestra número y su representación binaria y con sus leds encendidos y apagados
+    print(f"{numero:2} -> {binario}  {leds}")
+    # se produce un retardo de 0.5 segundos para simular el conteo
     time.sleep(0.5)
