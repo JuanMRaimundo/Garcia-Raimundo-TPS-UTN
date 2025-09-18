a9afbe2d91d210547ae1ca2ec9bc1bb1bcb5b5c3
Supernose264 <bautigarcia264@gmail.com>
2025-09-18 16:30:31 -0300

    Agrego el codigo
    Hice el codigo añadiendo etiquetas para que sea facil entender el codigo linea por linea

--- TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
+++ TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
@@ -1,1 +1,10 @@
-import time
+import time  # Para usar sleep
+
+# Cuenta del 0 al 15
+for numero in range(16):
+    # Convertierte a binario con formato de 4 bits
+    binario = format(numero, '04b')
+    # Muestra número y su representación binaria
+    print(f"{numero} -> {binario}")
+    # SE produce un retardo de 0.5 segundos para simular el conteo
+    time.sleep(0.5)
