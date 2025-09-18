ba9c936c562fccf33bf1aa54ec97582acf85c31b
Supernose264 <bautigarcia264@gmail.com>
2025-09-18 16:44:55 -0300

    Sistema Hexadecimal
    Agregue el sistema hexadecimal a la cuenta

--- TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
+++ TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
@@ -5,10 +5,11 @@ for numero in range(16):
     # Representaciones en distintos sistemas
     binario = format(numero, '04b')   # Binario con 4 bits
     octal = format(numero, 'o')       # Octal
+    hexadecimal = format(numero, 'X') # Hexadecimal en mayúsculas
     #Esto crea la ilusion de led encendidos y apagados, esto muestran de manera visual los encendidos y apagados del codigo 
     #🔴 para 1 y  ⚫ para 0
     leds = ''.join('🔴' if bit == '1' else '⚫' for bit in binario)
-    #Muestra número y su representación binaria y con sus leds encendidos y apagados
-    print(f"{numero:2} -> {binario}  {leds} | Oct: {octal:>2} | ")
+    #Muestra número y su representación binaria, octal y hexadecimal y con sus leds encendidos y apagados
+    print(f"{numero:2} -> {binario}  {leds} | Oct: {octal:>2} | Hex: {hexadecimal:>2}")
     # se produce un retardo de 0.5 segundos para simular el conteo
     time.sleep(0.5)
