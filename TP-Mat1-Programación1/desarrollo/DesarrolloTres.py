bb17192c4d90e51a14f9b653e601f49a5c7254a0
Supernose264 <bautigarcia264@gmail.com>
2025-09-18 16:42:20 -0300

    Sistema octal
    Agregue El formato octal para que también se muestre la cuenta pero en el sistema octal

--- TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
+++ TP-Mat1-Programación1/desarrollo/DesarrolloTres.py
@@ -2,12 +2,13 @@ import time  # Para usar sleep
 
 # Cuenta del 0 al 15
 for numero in range(16):
-    # Convertierte a binario con formato de 4 bits
-    binario = format(numero, '04b')
+    # Representaciones en distintos sistemas
+    binario = format(numero, '04b')   # Binario con 4 bits
+    octal = format(numero, 'o')       # Octal
     #Esto crea la ilusion de led encendidos y apagados, esto muestran de manera visual los encendidos y apagados del codigo 
     #🔴 para 1 y  ⚫ para 0
     leds = ''.join('🔴' if bit == '1' else '⚫' for bit in binario)
     #Muestra número y su representación binaria y con sus leds encendidos y apagados
-    print(f"{numero:2} -> {binario}  {leds}")
+    print(f"{numero:2} -> {binario}  {leds} | Oct: {octal:>2} | ")
     # se produce un retardo de 0.5 segundos para simular el conteo
     time.sleep(0.5)
