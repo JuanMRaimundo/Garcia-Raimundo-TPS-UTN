c59d031261a9c202b46c3737892c787097d2a287
Supernose264 <bautigarcia264@gmail.com>
2025-09-19 16:15:46 -0300

    A y b Interactivo
    Agregue Las opciones, para que el usuario ponga el valor de 0 y 1 y se validen

--- TP-Mat1-Programación1/desarrollo/DesarrolloSiete.py
+++ TP-Mat1-Programación1/desarrollo/DesarrolloSiete.py
@@ -8,3 +8,22 @@ for A in [0, 1]:
         suma  = A ^ B   # XOR para el bit de suma
         carry = A & B   # AND para el acarreo
         print(f" {A} | {B} |  {suma}   |   {carry}")
+#Esto valida que el numero es 0 o 1
+while True:
+    #el usuario ingresa el valor de A
+    A_str=input("\nIngresa A (0 o 1): ").strip()
+    #si A_str no es 0 o 1 imprime mensaje de error y vuelve a pedir el numero
+    if A_str not in ("0", "1"):
+        print("  Entrada inválida. Debes ingresar 0 o 1.")
+        continue
+    #si la entreda es valida convierte A_str a entero y lo asigna a A
+    A = int(A_str)
+    #el usuario ingresa el valor de B
+    B_str = input("Ingresa B (0 o 1): ").strip()
+    #si B_str no es 0 o 1 imprime mensaje de error y vuelve a pedir el numero
+    if B_str not in ("0", "1"):
+        print("  Entrada inválida. Debes ingresar 0 o 1.")
+        continue
+    #si la entreda es valida convierte B_str a entero y lo asigna a B
+    B = int(B_str)
+
