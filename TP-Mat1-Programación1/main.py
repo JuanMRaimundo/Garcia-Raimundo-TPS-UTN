bceef22bce92f08c90e16f1015dc3652bdafdf7b
Juan Marco Raimundo <juanmr.093@gmail.com>
2025-10-11 19:48:01 -0300

    Actualizacion

--- TP-Mat1-Programación1/main.py
+++ TP-Mat1-Programación1/main.py
@@ -50,6 +50,7 @@ while True:
             print("1. AND    2. OR    3. NOT")
             print("4. SALIR") 
             opcion = input("Seleccione una opción (1-4): ")
+
             #Este while nos permitirá validar continuamente las opciones ingresadas dentro de este menú
             while opcion not in ["1", "2", "3", "4"]:
                 print("Opción incorrecta! Por favor, ingrese 1, 2, 3 o 4.")
@@ -57,9 +58,12 @@ while True:
                 print("1. AND    2. OR    3. NOT")
                 print("4. SALIR") 
                 opcion = input("Seleccione una opción (1-4): ")
+
+            #Salimos del while y se corta el programa    
             if opcion == "4":
                 print("Nos vemos pronto, hasta luego!")
                 break
+
             if opcion == "3":
                 valor = input("Usted ingresó a la compuerta NOT. Por favor, ingrese el valor binario (1 o 0): ")
                 while valor not in ['0', '1']:
@@ -72,6 +76,8 @@ while True:
                 else:
                     resultado = 1
                 print(f"COMPUERTA NOT({a}) = {resultado}")    
+
+                
             elif opcion in ["1","2"]:
                 valor1 = input("Ingrese el primer valor binario (0 o 1): ")
                 while valor1 not in ['0', '1']:
@@ -82,13 +88,15 @@ while True:
                     print("Error: Debe ingresar 0 o 1.")
                     valor2 = input("Ingrese el segundo valor binario (0 o 1): ")
                 a = int(valor1)
-                b = int(valor2)          
+                b = int(valor2)   
+
                 if opcion == "1":  # COMPUERTA AND
                     if a == 1 and b == 1:
                         resultado = 1
                     else:
                         resultado = 0
                     print(f"COMPUERTA AND: ({a}), ({b}) = {resultado}")  
+                    
                 if opcion == "2":  # COMPUERTA OR
                     if a == 0 and b == 0:
                         resultado = 0
