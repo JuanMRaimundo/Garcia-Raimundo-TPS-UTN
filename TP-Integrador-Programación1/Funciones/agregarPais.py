004442e1f95ade8e926bdeb50e02da2791e34fdf
Juan Marco Raimundo <juanmr.093@gmail.com>
2025-11-02 17:33:36 -0300

    Agregando función 'Agregar país'

--- TP-Integrador-Programación1/Funciones/agregarPais.py
+++ TP-Integrador-Programación1/Funciones/agregarPais.py
@@ -1,1 +1,17 @@
-def agregarPais()
+
+
+def agregarPais(lista_paises):
+    print("\n ## A continuación podrá agregar países: ##\n")
+    cantidad_paises=int(input("Ingrese la cantidad de países que desea agregar: "))
+    for i in range(cantidad_paises):
+        print(f"\n--- País {i+1} ---")
+        nombre=input("Ingrese el nombre: ").upper()
+        ##AGREGAR VALIDACIÓN SI EXISTE O NO
+        poblacion=int(input("Ingrese la población: "))
+        superficie=float((input("Ingrese la superficie: ")))
+        continente=input("Ingrese el continente: ").upper()
+        lista_paises.append({"nombre":nombre,
+                       "poblacion":poblacion,
+                       "superficie":superficie,
+                       "continente":continente})
+    print("Los países han sido agregados correctamente")    
